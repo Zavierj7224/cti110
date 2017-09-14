880276107a4ec433a5683817b824210acc634561
Zavierj7224 <31966361+Zavierj7224@users.noreply.github.com>
2017-09-14 12:57:30 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_JonesZavier.py
@@ -0,0 +1,17 @@
+#CTI 110
+#M2T1-Sales Prediction
+#Zavier Jones
+#September 13th, 2017
+#Predicting my total sales of 23 percent
+
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Displaying the profit.
+print('The profit is $', profit)
+
+      
